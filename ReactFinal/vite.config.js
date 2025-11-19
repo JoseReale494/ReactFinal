3ec092476214754c2426af516d952bdf1b5a32a4
JoseReale494 <jose.reale@mi.unc.edu.ar>
2025-11-18 22:06:24 -0300

    Build para GitHub Pages

--- ReactFinal/vite.config.js
+++ ReactFinal/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactFinal/',
+  base: '/ReactFinal/', // nombre EXACTO de tu repositorio
 })
+
