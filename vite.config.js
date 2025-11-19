01635b2f5684e11ae64def20dce6d0956a0fd025
JoséReae494 <jose.reale@mi.unc.edu.ar>
2025-11-18 22:29:47 -0300

    Configurar Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ReactFinal/",
   plugins: [react()],
-  base: '/ReactFinal/', // nombre EXACTO de tu repositorio
-})
-
+});
